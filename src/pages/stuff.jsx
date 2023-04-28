f6786c71c18bca2d048cfd267eefa37083d9de74
wan-mureithi <estherdee27@gmail.com>
2023-04-28 19:40:27 +0300

    additions

---
+++ src/pages/stuff.jsx
@@ -0,0 +1,247 @@
+import React from 'react'
+
+const Stuff = () => {
+  return (
+    <div>
+        <section class="ws-section-spacing blog-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="center-title ">
+                        <h2 class="title">Travel guide and Expert Advice</h2>
+                        <h4 class="sub-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua minim veniam.</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="package-one blog-box">
+                        <div class="img-wapper">
+                            <img src="images/pack-4.jpg" alt="package-img"/>
+                        </div>
+                        <div class="package-content">
+                            <h3><a href="blog-details.html">Eos ut takimata dolor dolor.</a></h3>
+                            <ul class="meta-tag">
+                                <li><a href="#"><i class="fa fa-user"></i> Jhon Doe</a></li>
+                                <li><a href="#"><i class="far fa-calendar-alt"></i> 12 june, 2019</a></li>
+                                <li><a href="#"><i class="fa fa-comments"></i> 10</a></li>
+                            </ul>
+                            <p>Erat erat ea amet et amet et lorem at labore invidunt, aliquyam gubergren duo duo dolor
+                                ipsum invidunt aliquyam diam.</p>
+                            <a href="#" class="read-btn">Read More <i class="fas fa-angle-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="package-one blog-box">
+                        <div class="img-wapper">
+                            <img src="images/pack-5.jpg" alt="package-img"/>
+                        </div>
+                        <div class="package-content">
+                            <h3><a href="blog-details.html">Eirmod ut sanctus diam sed.</a></h3>
+                            <ul class="meta-tag">
+                                <li><a href="#"><i class="fa fa-user"></i> Jhon Doe</a></li>
+                                <li><a href="#"><i class="far fa-calendar-alt"></i> 15 june, 2019</a></li>
+                                <li><a href="#"><i class="fa fa-comments"></i> 10</a></li>
+                            </ul>
+                            <p>Erat erat ea amet et amet et lorem at labore invidunt, aliquyam gubergren duo duo dolor
+                                ipsum invidunt aliquyam diam.</p>
+                            <a href="#" class="read-btn">Read More <i class="fas fa-angle-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="package-one blog-box">
+                        <div class="img-wapper">
+                            <img src="images/pack-6.jpg" alt="package-img"/>
+                        </div>
+                        <div class="package-content">
+                            <h3><a href="blog-details.html">Eirmod diam amet diam clita</a></h3>
+                            <ul class="meta-tag">
+                                <li><a href="#"><i class="fa fa-user"></i> Jhon Doe</a></li>
+                                <li><a href="#"><i class="far fa-calendar-alt"></i> 20 june, 2019</a></li>
+                                <li><a href="#"><i class="fa fa-comments"></i> 10</a></li>
+                            </ul>
+                            <p>Erat erat ea amet et amet et lorem at labore invidunt, aliquyam gubergren duo duo dolor
+                                ipsum invidunt aliquyam diam.</p>
+                            <a href="#" class="read-btn">Read More <i class="fas fa-angle-double-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {/* end travel guide  */}
+    <section class="ws-section-spacing bg-gray">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="place-details">
+                        <div class="wapper-img">
+                            <img src="images/place.jpg" alt="place"/>
+                        </div>
+                        <div class="place-content">
+                            <h3>AMERICA : Loss Angeles California</h3>
+                            <p>Justo sea ipsum sit justo voluptua ea et est. Consetetur clita diam clita dolor diam,
+                                elitr sanctus magna ut diam gubergren elitr sed dolores. Accusam sea duo takimata sed,
+                                ipsum no consetetur et sea. Rebum justo et sea eos eos takimata sanctus sit gubergren.
+                                Et lorem lorem consetetur aliquyam lorem nonumy aliquyam clita erat, kasd tempor sea
+                                consetetur diam stet ut. Ea dolore sadipscing elitr et dolores amet elitr.ipsum diam
+                                vero est dolore. Consetetur aliquyam eirmod et et et gubergren, amet voluptua voluptua
+                                sea sit magna dolor sed, sed lorem at nonumy magna.Ut et dolor vero est ipsum, sanctus
+                                magna clita ipsum accusam ut sit ut, ea dolor sea sit diam nonumy,ipsum dolor voluptua
+                                consetetur diam duo.</p>
+                            <p>Rebum aliquyam dolor ipsum stet est magna sea eirmod, Invidunt ipsum justo rebum erat
+                                rebum et. Labore labore amet vero et est. Accusam sit justo.Vero rebum tempor dolore et
+                                est kasd. Justo diam no lorem no, duo aliquyam diam sea accusam elitr. Accusam magna
+                                clita dolor dolor,dolor at dolor accusam dolores elitr justo dolor accusam nonumy. magna
+                                dolor magna eirmod .</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="servicebox-one ">
+                        <div class="service-box-icon"><i class="fas fa-route"></i></div>
+                        <h3 class="service-box-title">Best Travel Agent</h3>
+                        <p class="service-box-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore .</p>
+                        <a class="btn" href="#">See more</a>
+                    </div>
+                </div>
+               
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="servicebox-one ">
+                        <div class="service-box-icon"><i class="fab fa-avianex"></i></div>
+                        <h3 class="service-box-title">Best Price Guarantee</h3>
+                        <p class="service-box-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore .</p>
+                        <a class="btn" href="#">See more</a>
+                    </div>
+                </div>
+               
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="servicebox-one ">
+                        <div class="service-box-icon"><i class="fas fa-bullhorn"></i></div>
+                        <h3 class="service-box-title">Trust &amp; Safety</h3>
+                        <p class="service-box-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                            eiusmod tempor incididunt ut labore et dolore .</p>
+                        <a class="btn" href="#">See more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+    <section class="same-section-spacing desination-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="center-title ">
+                        <h2 class="title">Top destination in the world</h2>
+                        <h4 class="sub-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua minim veniam.</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="destination-slide">
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-1.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>florida sea </h4>
+                                    <p>Miami Beach</p>
+                                    <span class="float-right">4 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-2.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>utah mountains </h4>
+                                    <p>sunset</p>
+                                    <span class="float-right">3 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-3.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>francisco</h4>
+                                    <p>california city</p>
+                                    <span class="float-right">6 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-4.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>Paris </h4>
+                                    <p>Eiffel Tower</p>
+                                    <span class="float-right">7 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-5.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>Italy </h4>
+                                    <p>canal City</p>
+                                    <span class="float-right">5 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="place-box">
+                                <div class="wapper-img">
+                                    <img src="images/des-6.jpg" alt=""/>
+                                    <a href="#" class="pop-btn">View All Tours</a>
+                                </div>
+
+                                <div class="desi-inner">
+                                    <h4>Australia </h4>
+                                    <p>City</p>
+                                    <span class="float-right">4 tours</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    </div>
+  )
+}
+
+export default Stuff
